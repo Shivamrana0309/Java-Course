eadfadc2c5f76e68096370060a075085e8d2b63b
Shivam <shivamrana_230029@aitpune.edu.in>
2025-08-28 14:50:49 +0530

    baiscs

--- oops.java
+++ oops.java
@@ -1,18 +1,34 @@
 class Calculator{
-    int a;
-    public int add(int num1,int num2){
+    int a; //instance variable
+    public int add(int num1,int num2){ // method
         // System.out.println("in add");
         int r=num1+num2;
         return r;
+    }
+
+    public int add(int n1, int n2, int n3){ // method overloading
+        return n1+n2+n3;
     }
 }
 
 public class oops {
     public static void main(String[] args) {
         // here cal is a ref variable 
-        Calculator cal = new Calculator();
-        int res = cal.add(4,5);
-        System.out.println(res);
+        // Calculator cal = new Calculator();
+        // int res = cal.add(4,5);
+        // System.out.println(res);
+
+        // array
+        // int nums[]={1,2,3,4}; // or int nums[]= new int[4]; -> bydefault all the values ae zero
+        // int a = nums[0];
+        // nums[2]=6;
+        // System.out.println(nums[2]);
+        // int nums[][] =new int[3][4] -> 2D Array
+        // int nums[][] =new int [3][] -> Jagged Array
+        // need to define like this -> nums[0] =new int[3];
+        // Drawbacks -> finite size and are of homogeneous datatypes
+
+        
     }
 }
 
