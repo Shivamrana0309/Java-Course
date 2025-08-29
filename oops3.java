978fa437a42cd93aaa015a29d60521f8ea31da20
Shivam <shivamrana_230029@aitpune.edu.in>
2025-08-29 11:40:35 +0530

    Encapsualtion and This Keyword

---
+++ oops3.java
@@ -0,0 +1,36 @@
+class Human{
+    // Encapsulation is binding of the class data with methods so that no-one of the outside world
+    // will be able to access that
+    // to use that data we need to define methods
+
+    private int age ;
+    private String name;
+    // private keyword ensures that the instance variables are not used outside of the class
+
+    public void setAge(int age){ // setter function
+        this.age=age; // this keyword is used to point towards the instance variable of the class
+    }
+    // or instead of using this keyword we can do it by
+    public void setAge(int age, Human obj){ // method overloading
+        obj.age = age;
+    }
+    public void setName(String s){ // setter function or method
+        this.name=s;
+    }
+    public int getAge(){ // getter method
+        return age;
+    }
+    public String getName(){ // getter method
+        return name;
+    }
+}
+
+public class oops3 {
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        h1.setAge(20,h1);
+        h1.setName("Shivam");
+        System.out.println("Name: "+h1.getName());
+        System.out.println("Age: "+h1.getAge());
+    }
+}
