34d4df41beb217f001c42c5afdfcc8e83797c952
Shivam <shivamrana_230029@aitpune.edu.in>
2025-09-01 23:04:23 +0530

    Inner-class, Anonymous class, Abstract and Anonymous class

--- oops11.java
+++ oops11.java
@@ -12,6 +12,10 @@ class Students{
     }
 }
 
+abstract class Classroom{
+    public abstract void show();
+}
+
 public class oops11 {
     public static void main(String[] args) {
 
@@ -21,5 +25,21 @@ public class oops11 {
         Students.Monitor obj1 = obj.new Monitor(); // this is how object of an inner-class is created
         // new keyword is used with an object ref variable because the inner-class is non-static one
         obj1.config();
+
+        Students obj2 = new Students(){
+            // to get new implementation of the show() method we can create it here
+            public void show(){ // this is anonymous inner-class
+                System.out.println("in new show");
+            }
+        };
+        obj2.show();
+
+        Classroom obj3 = new Classroom() {
+            // here an abstract class's object can also be created using this anonymous inner-class
+            public void show(){
+                System.out.println("in classroom");
+            }
+        };
+        obj3.show();
     }
 }
